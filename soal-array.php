1366ee11b3ea0e35dada79fab1e8f1b255efbfe9
laelydp <laelydwiyanap@gmail.com>
2022-10-10 20:19:12 +0700

    Tugas PWB

--- soal-array.php
+++ soal-array.php
@@ -19,29 +19,41 @@
             Kids : "Mike", "Dustin", "Will", "Lucas", "Max", "Eleven" 
             Adults: "Hopper", "Nancy",  "Joyce", "Jonathan", "Murray"
         */
-    $kids = array("Mike", "Dustin", "Will", "Lucas", "Max", "Eleven"); // Lengkapi di sini
-    $adults;
+    $kids = array("Mike", "Dustin", "Will", "Lucas", "Max", "Eleven"); 
+    print_r ($kids);
+    echo "<br>";
+    $adults = array("Hopper", "Nancy",  "Joyce", "Jonathan", "Murray");
+    print_r ($adults);
+
     echo "<h3> Soal 2</h3>";
     /* 
             SOAL NO 2
             Tunjukkan panjang Array di Soal No 1 dan tampilkan isi dari masing-masing Array.
         */
-    echo "Cast Stranger Things: ";
+    echo "Cast Stranger Things: " ;
     echo "<br>";
     echo "Total Kids: " . count($kids); // Berapa panjang array kids
     echo "<br>";
     echo "<ol>";
     echo "<li> $kids[0] </li>";
-    // Lanjutkan
-
+    echo "<li> $kids[1] </li>";
+    echo "<li> $kids[2] </li>";
+    echo "<li> $kids[3] </li>";
+    echo "<li> $kids[4] </li>";
+    echo "<li> $kids[5] </li>";
+    
     echo "</ol>";
 
-    echo "Total Adults: "; // Berapa panjang array adults
+    echo "Total Adults: " . count($adults); // Berapa panjang array adults
     echo "<br>";
     echo "<ol>";
     echo "<li> $adults[0] </li>";
-    // Lanjutkan
-
+    echo "<li> $adults[0] </li>";
+    echo "<li> $adults[1] </li>";
+    echo "<li> $adults[2] </li>";
+    echo "<li> $adults[3] </li>";
+    echo "<li> $adults[4] </li>";
+    
     echo "</ol>";
 
     /*
@@ -107,6 +119,34 @@
                 )
             
         */
+        echo "<h3> Soal 3</h3>";  
+        $aktor = array(
+        //data data untuk array    
+			array( "Name"=>"Will Byers", 
+					"Age"=>"12",
+					"Aliases"=>"Will the Wise",
+                    "Status"=>"Alive"),
+
+			array( "Name"=>"Mike Wheeler", 
+					"Age"=>"12",
+					"Aliases"=>"Dugeon Master",
+                    "Status"=>"Alive"),
+
+			array( "Name"=>"Jim Hooper", 
+					"Age"=>"43",
+					"Aliases"=>"Chief Hopper",
+                    "Status"=>"Deceased"),
+
+			array( "Name"=>"Eleven", 
+					"Age"=>"12",
+					"Aliases"=>"El",
+                    "Status"=>"Alive")
+
+		);
+/* tampilkan variabel array */
+echo '<pre>';
+print_r($aktor); 
+echo '</pre>';
     ?>
 </body>
 
--- soal-string.php
+++ soal-string.php
@@ -32,10 +32,13 @@
 
         
         echo "<h3> Soal No 1</h3>";
-
+ 
         $first_sentence = "Hello PHP!" ; // Panjang string 10, jumlah kata: 2
         $second_sentence = "I'm ready for the challenges"; // Panjang string: 28,  jumlah kata: 5
-        
+            echo "Panjang string : " . strlen($first_sentence) . ", " . "Jumlah kata : " . str_word_count($first_sentence) . " " . "<strong>Dari kata" . " " . ($first_sentence) . "</strong>";
+            echo "<br>";
+            echo "Panjang string : " . strlen($second_sentence) . ", " . "Jumlah kata : " . str_word_count($second_sentence) . " "  . "<strong>Dari kata" . " " . ($second_sentence) . "</strong>";
+
         echo "<h3> Soal No 2</h3>";
         /* 
             SOAL NO 2
@@ -48,8 +51,9 @@
         echo "<label>String: </label> \"$string2\" <br>";
         echo "Kata pertama: " . substr($string2, 0, 1) . "<br>" ; 
         // Lanjutkan di bawah ini
-        echo "Kata kedua: " ;
-        echo "<br> Kata Ketiga: " ;
+        echo "Kata kedua: " . substr($string2,2,4); 
+        echo "<br> Kata Ketiga: " . substr($string2,6,4);
+
         echo "<h3> Soal No 3 </h3>";
         /*
             SOAL NO 3
@@ -57,6 +61,8 @@
         */
         $string3 = "PHP is old but Good!";
         echo "String: \"$string3\" "; 
+        echo "<br>";
+        echo str_replace ("Good!", "awesome", $string3);
         // OUTPUT : "PHP is old but awesome"
 
     ?>
